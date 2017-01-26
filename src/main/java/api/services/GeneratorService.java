72818f5b492d0bdade6bb1a39de3218375acab31
Dylan van Gils <dylanvgils@users.noreply.github.com>
2017-01-26 11:47:10 +0100

    Created rule generation syntax
    Changed access modifier
    
    Shortend name

---
+++ src/main/java/api/services/GeneratorService.java
@@ -0,0 +1,47 @@
+package api.services;
+
+import businessLogic.RuleFacade;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import java.util.ArrayList;
+import java.util.List;
+
+@Path("/generator")
+@Produces(MediaType.APPLICATION_JSON)
+public class GeneratorService {
+    @POST
+    @Path("/rules")
+    public Response generateRules(String json) {
+        Status status = Status.OK;
+        JSONObject jObject;
+        List<Integer> rules = new ArrayList<>();
+        String lang = null;
+
+        try {
+            jObject = new JSONObject(json);
+            JSONArray jArray = jObject.getJSONArray("rules");
+            lang = jObject.getString("lang");
+
+            for (int i = 0; i < jArray.length(); i ++) {
+                rules.add(jArray.getInt(i));
+            }
+        } catch (JSONException e) {
+            status = Status.INTERNAL_SERVER_ERROR;
+            jObject = new JSONObject()
+                     .put("message", "Unable to parse the provided JSON. Check your JSON syntax.");
+        }
+
+        RuleFacade.generateRules(lang, rules);
+        jObject.put("message", "Rules generated succesfully!");
+
+        return Response.status(status).entity(jObject.toString()).build();
+    }
+}
